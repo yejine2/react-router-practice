33fd23fb24f9ab7ee22f5f6593529e811fd19843
yejine2 <yejine2@naver.com>
2022-08-05 20:50:41 +0900

    feat: Add pathname

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import Posts from './components/Posts'
 import PostDetail from './components/Posts/PostDetail'
 import Users from './components/Users'
@@ -7,8 +7,13 @@ import UserDetail from './components/Users/UserDetail'
 import UserIndex from './components/Users/UserIndex'
 
 function App() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
   return (
     <div>
+      <h5>{location.pathname}</h5>
+      <button onClick={() => navigate(-1)}>뒤로가기</button>
       <nav>
         <Link to="/posts">Posts</Link> | <Link to="/users">Users</Link>
       </nav>
